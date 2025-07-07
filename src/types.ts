7de5034010319552c25372fb3e2302c9eeda006d
Pavlo Baliuk <pv.baliuk@gmail.com>
2025-07-07 07:15:38 +0300

    Remove `Jsonifiable` types from `types.ts` as they are no longer in use

--- src/types.ts
+++
@@ -1,5 +0,0 @@
-type JsonPrimitive = string | number | boolean | null;
-type JsonifiableObject = {[Key in string]?: Jsonifiable} | {toJSON: () => Jsonifiable};
-type JsonifiableArray = readonly Jsonifiable[];
-
-export type Jsonifiable = JsonPrimitive | JsonifiableObject | JsonifiableArray;
