e648feab19e7772f6fe6a992d238d82caca88cf8
Pavlo Baliuk <pv.baliuk@gmail.com>
2025-10-25 08:51:52 +0300

    Fix resultify function

--- src/resultify.ts
+++ src/resultify.ts
@@ -1,29 +1,24 @@
-export type ErrorResult<R, E = Error> = readonly [R, null] | readonly [null, E];
+export type ErrorResult<R, E> = readonly [R, null] | readonly [null, E];
 
-export function resultify<E = Error, R = any>(fn: () => R): ErrorResult<R, E>;
-export function resultify<E = Error, R = any>(fn: () => Promise<R>): Promise<ErrorResult<R, E>>;
 /**
  * Executes a function and returns its result as a tuple `[value, error]`.
  * Works with both sync and async functions. Never throws.
  *
+ * @template {() => any} F
  * @template {Error} E
- * @template {any} R
- * @param {(() => R) | (() => Promise<R>)} fn
- * @returns {ErrorResult<R> | Promise<ErrorResult<R, E>>}
+ * @param {F} fn
  */
-export function resultify<E = Error, R = any>(
-    fn: () => R | Promise<R>
-): ErrorResult<R, E> | Promise<ErrorResult<R, E>> {
+export function resultify<F extends () => any, E = Error>(
+    fn: F
+): ReturnType<F> extends Promise<infer R> ? Promise<ErrorResult<R, E>> : ErrorResult<ReturnType<F>, E> {
     try {
         const promiseOrResult = fn();
         if (promiseOrResult instanceof Promise) {
-            return (promiseOrResult as Promise<R>)
-                .then(res => [res as R, null] as const)
-                .catch(err => [null, err as E] as const);
+            return promiseOrResult.then(res => [res, null] as const).catch(err => [null, err as E] as const) as any;
         }
 
-        return [promiseOrResult as R, null] as const;
+        return [promiseOrResult, null] as const as any;
     } catch (e) {
-        return [null, e as E] as const;
+        return [null, e as E] as const as any;
     }
 }
